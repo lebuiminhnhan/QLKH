04bc1cd78944bb8269ef3f394d49e25ce4c29dc2
lebuiminhnhan <37339269+lebuiminhnhan@users.noreply.github.com>
2019-01-01 15:55:18 +0700

    no message

--- QuanLiKhachHang/QuanLiKhachHang/MyUserControl/UserControlBar.xaml.cs
+++ QuanLiKhachHang/QuanLiKhachHang/MyUserControl/UserControlBar.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLiKhachHang.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace QuanLiKhachHang.MyUserControl
     /// </summary>
     public partial class UserControlBar : UserControl
     {
+        public ControlBarViewModel Viewmodel { get; set; }
+
         public UserControlBar()
         {
             InitializeComponent();
+            this.DataContext = Viewmodel = new ControlBarViewModel();
         }
     }
 }
--- QuanLiKhachHang/QuanLiKhachHang/ViewModel/MainViewModel.cs
+++ QuanLiKhachHang/QuanLiKhachHang/ViewModel/MainViewModel.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLiKhachHang.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+
+        // mọi thứ xử lý sẽ nằm trong này
         public MainViewModel()
         {
-           
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                Isloaded = true;
+                LoginForm loginWindow = new LoginForm();
+                loginWindow.ShowDialog();
+            }
+              );
         }
-       
+
     }
 }
